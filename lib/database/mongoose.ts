8ab30d3450a00a5bcb6ad6518dc1171adfbe6671
Yuji Sakaguchi <yujisakaguchi@ucsb.edu>
2024-03-26 16:02:38 -0700

    trying to fix

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,32 +1,30 @@
 import mongoose, { Mongoose } from 'mongoose';
 
+// Define the MongoDB URL using environment variables
 const MONGODB_URL = process.env.MONGODB_URL;
 
+// Define the structure of the MongooseConnection interface
 interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+    conn: Mongoose | null; // Mongoose connection or null if not initialized
+    promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+// Next.js runs serverless so each request is handled independetly allowing for better scalability and reliability
+// No need to manage persistent connections between many instances
 
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+// Global variable to cache the Mongoose connection and promise
+let cached: MongooseConnection = (global as any).mongoose 
+if (!cached) { // If the Mongoose connection is not cached, initialize it
+    cached = (global as any).mongoose = {
+        conn: null, promise: null
+    }
 }
 
+// Function to connect to the MongoDB database
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
-
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
-    })
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+    if (cached.conn) return cached.conn;
+    if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { dbName: "imaginify", bufferCommands: false })
+    cached.conn = await cached.promise; // Wait for the Mongoose connection promise to resolve and set the connection
+    return cached.conn;
 }
