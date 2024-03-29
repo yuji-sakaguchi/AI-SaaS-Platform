386f81637086d1fcc3847f3f2d842fb9ae901f29
Yuji Sakaguchi <yujisakaguchi@ucsb.edu>
2024-03-29 14:23:39 -0700

    added titles, subtitles

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,16 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm';
 
-const AddTransformationTypePage = () => { // Add Transformation Page
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => { // Add Transformation Page
+  const transformation = transformationTypes[type];
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm />
+    </>
+
   )
 }
 
